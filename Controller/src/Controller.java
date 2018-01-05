5ee8f16ad4bb95322a85b5b1174b71187cf03900
nodnaba <1119196803@qq.com>
2018-01-05 10:43:56 +0800

    最终版controller

--- Controller/src/Controller.java
+++ Controller/src/Controller.java
@@ -3,6 +3,7 @@ import struct.*;
 import struct.Event;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -14,11 +15,23 @@ public class Controller extends JFrame {
     final int  MAXSIZE = 100;
 
     JPanel splitePane = new JPanel();
-    Event[] event = new Event[MAXSIZE];
-    String proj;
-    String mainRefereeUser;
-    String[] refereeUser = new String[5];
-    Referee[] freeRefereeList;
+
+    //********************下拉框选中内容
+    int proj;    // 项目编号
+    AgeGroup selectedAge;   // 年龄组
+    String mainRefereeUser; // 主裁判
+    Vector<String> refereeUser = new Vector<>();   // 裁判
+
+
+    //*******************发送内容
+    Vector<Event> freeEventList;    // 项目列表
+    AgeGroup[] age = {  new AgeGroup(7,8),
+            new AgeGroup(9,10),
+            new AgeGroup(11,12)};  // 年龄组
+    Vector<String> freeRefereeList;    // 空闲裁判列表
+    Vector<OrderInfo> sendOrderInfo;    // 出场顺序列表
+
+    // socket
     Socket client;
     ObjectOutputStream out;
     ObjectInputStream in;
@@ -26,7 +39,7 @@ public class Controller extends JFrame {
 
     public Controller() {
         try {
-            client = new Socket("192.168.137.1", 2048);
+            client = new Socket("10.133.174.11", 2048);
 
             out = new ObjectOutputStream(client.getOutputStream());
             in = new ObjectInputStream(client.getInputStream());
@@ -35,17 +48,18 @@ public class Controller extends JFrame {
             e1.printStackTrace();
         }
             this.setSize(700, 700);
-            //getEvent();
+            getEvent();
             getFree();
-            /*initPane();
+            //getOrder();
+            initPane();
             this.add(splitePane);
             this.setTitle("小组控制人员");
             this.setResizable(true);
-            this.setVisible(true);*/
+            this.setVisible(true);
             while(true);
-
     }
 
+    //*************************获取项目列表
     public void getEvent(){
         // 发
         try {
@@ -56,34 +70,21 @@ public class Controller extends JFrame {
             e1.printStackTrace();
         }
 
-        try {
-            String msg = (String)in.readObject();
-            System.out.println(msg);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        /*
         // 收
         try {
-            while(true) {
-                TcpType head = TcpType.valueOf((String)in.readObject());
-
-                if(in.readObject() == "EventList" && in.readBoolean()) {
-                    freeRefereeList = (Referee[])in.readObject();   // 获取空闲列表
-                }
+            TcpType head = TcpType.valueOf((String)in.readObject());
+            if(head == TcpType.EventList){
+                freeEventList = (Vector<Event>)in.readObject();   // 获取空闲列表
             }
-
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }catch(ClassNotFoundException cnfe) {
             System.out.println(cnfe);
-        }*/
+        }
     }
 
+    //*************************获取空闲裁判列表
     public void getFree(){
         // 发
         try {
@@ -93,44 +94,70 @@ public class Controller extends JFrame {
             e1.printStackTrace();
         }
 
+        // 收
         try {
             TcpType head = TcpType.valueOf((String)in.readObject());
-            Vector<String> freelist = null;
-            if(head == TcpType.FreeList)
-                freelist = (Vector<String>) in.readObject();
-            System.out.println(freelist);
-
+            if(head == TcpType.FreeList) {
+                freeRefereeList = (Vector<String>) in.readObject();
+                System.out.println(freeRefereeList);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        /*
-        // 收
+    }
+
+    //*************************获取出场信息
+    public void getOrder(){
+        // 发
         try {
-            TcpType head = TcpType.valueOf((String)in.readObject());
-
-            if(in.readObject() == "FreeList" && in.readBoolean()) {
-                freeRefereeList = (Referee[])in.readObject();   // 获取空闲列表
-
-            }
+            out.writeObject(TcpType.GetOrderInfo.toString());
+            out.writeInt(proj);
+            out.writeObject(selectedAge);
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
-        }catch(ClassNotFoundException cnfe) {
-            System.out.println(cnfe);
-        }*/
+        }
+
+        // 收
+        try {
+            TcpType head = TcpType.valueOf((String)in.readObject());
+            if(head == TcpType.OrderInfo) {
+                sendOrderInfo = (Vector<OrderInfo>) in.readObject();
+                System.out.println(sendOrderInfo);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void initPane() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try {
+                    in.close();
+                    out.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
         this.setBounds(500,500,700,500);
 
         JLabel label_project = new JLabel("选择项目");
         label_project.setBounds(110,10,150,30);
 
         JLabel label_age = new JLabel("选择年龄组");
-        label_age.setBounds(110,210,150,30);
+        label_age.setBounds(110,130,150,30);
+
+        JLabel label_chiefReferee = new JLabel("选择主裁判");
+        label_chiefReferee.setBounds(100,230,150,30);
 
         JLabel label_referee1 = new JLabel("选择裁判1");
         label_referee1.setBounds(300,10,150,30);
@@ -147,128 +174,104 @@ public class Controller extends JFrame {
         JLabel label_referee5 = new JLabel("选择裁判5");
         label_referee5.setBounds(300,330,150,30);
 
-        JLabel label_chiefReferee = new JLabel("选择主裁判");
-        label_chiefReferee.setBounds(500,170,150,30);
 
         JComboBox comboBox_project = new JComboBox();
-        comboBox_project.addItem("----请选择----");
-        comboBox_project.addItem("项目1");
-        comboBox_project.addItem("项目2");
-        comboBox_project.addItem("项目3");
+        comboBox_project.addItem("----------------请选择----------------");
+        for (int i = 0;i < freeEventList.size();i++) {
+            comboBox_project.addItem(freeEventList.get(i).getEventNo()
+                    + ")  " + freeEventList.get(i).getEventName()
+                    + "  " + freeEventList.get(i).getSex());
+        }
         comboBox_project.setBounds(50,50,200,30);
 
         JComboBox comboBox_age = new JComboBox();
         comboBox_age.addItem("----请选择----");
-        comboBox_age.addItem("年龄组1");
-        comboBox_age.addItem("年龄组2");
-        comboBox_age.addItem("年龄组3");
-        comboBox_age.setBounds(100,250,100,30);
+        for (int i = 0;i < age.length;i++) {
+            comboBox_age.addItem((i + 1) + ")  " + age[i].getAgeMin()
+                    + " ~ " + age[i].getAgeMax() + "岁");
+        }
+        comboBox_age.setBounds(100,170,100,30);
+
+        // 主裁判
+        JComboBox comboBox_chiefReferee = new JComboBox();
+        comboBox_chiefReferee.addItem("----请选择----");
+        for (int i = 0;i< freeRefereeList.size();i++) {
+            comboBox_chiefReferee.addItem(freeRefereeList.get(i));
+        }
+        comboBox_chiefReferee.setBounds(70,260,150,30);
 
         JComboBox comboBox_referee1 = new JComboBox();
         comboBox_referee1.addItem("----请选择----");
-       /* for(int i = 0;i < freeRefereeList.length;i++)
-            comboBox_referee1.addItem(freeRefereeList[i].getName());*/
-            /*
-        comboBox_referee1.addItem("裁判1");
-        comboBox_referee1.addItem("裁判2");
-        comboBox_referee1.addItem("裁判3");
-        */
+        for (int i = 0;i < freeRefereeList.size();i++) {
+            comboBox_referee1.addItem(freeRefereeList.get(i));
+        }
         comboBox_referee1.setBounds(300,50,100,30);
+
 
         JComboBox comboBox_referee2 = new JComboBox();
         comboBox_referee2.addItem("----请选择----");
-        comboBox_referee2.addItem("裁判1");
-        comboBox_referee2.addItem("裁判2");
-        comboBox_referee2.addItem("裁判3");
+        for (int i = 0;i < freeRefereeList.size();i++) {
+            comboBox_referee2.addItem(freeRefereeList.get(i));
+        }
         comboBox_referee2.setBounds(300,125,100,30);
 
         JComboBox comboBox_referee3 = new JComboBox();
         comboBox_referee3.addItem("----请选择----");
-        comboBox_referee3.addItem("裁判1");
-        comboBox_referee3.addItem("裁判2");
-        comboBox_referee3.addItem("裁判3");
+        for (int i = 0;i < freeRefereeList.size();i++) {
+            comboBox_referee3.addItem(freeRefereeList.get(i));
+        }
         comboBox_referee3.setBounds(300,200,100,30);
 
         JComboBox comboBox_referee4 = new JComboBox();
         comboBox_referee4.addItem("----请选择----");
-        comboBox_referee4.addItem("裁判1");
-        comboBox_referee4.addItem("裁判2");
-        comboBox_referee4.addItem("裁判3");
+        for (int i = 0;i < freeRefereeList.size();i++) {
+            comboBox_referee4.addItem(freeRefereeList.get(i));
+        }
         comboBox_referee4.setBounds(300,275,100,30);
 
         JComboBox comboBox_referee5 = new JComboBox();
         comboBox_referee5.addItem("----请选择----");
-        comboBox_referee5.addItem("裁判1");
-        comboBox_referee5.addItem("裁判2");
-        comboBox_referee5.addItem("裁判3");
+        for (int i = 0;i < freeRefereeList.size();i++) {
+            comboBox_referee5.addItem(freeRefereeList.get(i));
+        }
         comboBox_referee5.setBounds(300,350,100,30);
 
-        JComboBox comboBox_chiefReferee = new JComboBox();
-        comboBox_chiefReferee.addItem("----请选择----");
-        comboBox_chiefReferee.addItem("主裁判1");
-        comboBox_chiefReferee.addItem("主裁判2");
-        comboBox_chiefReferee.addItem("主裁判3");
-        comboBox_chiefReferee.setBounds(500,200,150,30);
-
-        JButton ensure = new JButton("确定");
+        JButton ensure = new JButton("打分指令");
         ensure.setBounds(500,350,100,30);
 
+        //****************************** 选择项目
         comboBox_project.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // 获取项目编号
-                try {
-                    TcpType head = TcpType.valueOf((String)in.readObject());
-
-                   if(in.readObject() == "EventList" && in.readBoolean()) {
-                       String[] projList = (String[])in.readObject();
-
-                       for(int i = 0;i < projList.length;i++)
-                           comboBox_project.addItem(projList[i]);
-
-                   }
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }catch(ClassNotFoundException cnfe) {
-                    System.out.println(cnfe);
-                }
-
-                proj = comboBox_project.getSelectedItem().toString();
-                System.out.println("fuck u bitch");
+                String a1 = comboBox_project.getSelectedItem().toString().substring(0,1);
+                String a2 = comboBox_project.getSelectedItem().toString().substring(1,2);
+                if(!a2.equals(")"))
+                    proj = 10 * Integer.parseInt(a1) + Integer.parseInt(a2);
+                else
+                    proj = Integer.parseInt(a1);
+                //System.out.println(proj);
             }
         });
 
+        //****************************** 选择年龄组
         comboBox_age.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // 获取年龄组编号
-                System.out.println("fuck u bitch");
+                int tmp = Integer.parseInt(comboBox_age.getSelectedItem().toString().substring(0,1));
+                selectedAge = age[tmp - 1];
             }
         });
 
-        // 选择裁判
+
+
+        //****************************** 选择裁判
         comboBox_referee1.addActionListener(new ActionListener() {
-            @Override
             public void actionPerformed(ActionEvent e) {
                 // 获取裁判1
-                try {
-                    TcpType head = TcpType.valueOf((String)in.readObject());
-                    if(in.readObject() == /*"FreeList"*/"GetFreeReferee" && in.readBoolean()) {
-                        String[] projList = (String[])in.readObject();
-
-                        for(int i = 0;i < projList.length;i++)
-                            comboBox_referee1.addItem(projList[i]);
-                    }
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }catch(ClassNotFoundException cnfe) {
-                    System.out.println(cnfe);
-                }
-
-                refereeUser[0] = comboBox_referee1.getSelectedItem().toString();
-                System.out.println("Referee1 is " + refereeUser[0]);
+                refereeUser.add(comboBox_referee1.getSelectedItem().toString());
+                System.out.println("Referee1 is " + refereeUser.get(0));
             }
         });
 
@@ -276,8 +279,8 @@ public class Controller extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // 获取裁判2
-                refereeUser[1] = comboBox_referee2.getSelectedItem().toString();
-                System.out.println("Referee2 is " + refereeUser[1]);
+                refereeUser.add(comboBox_referee2.getSelectedItem().toString());
+                System.out.println("Referee2 is " + refereeUser.get(1));
             }
         });
 
@@ -285,8 +288,8 @@ public class Controller extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // 获取裁判3
-                refereeUser[2] = comboBox_referee3.getSelectedItem().toString();
-                System.out.println("Referee3 is " + refereeUser[2]);
+                refereeUser.add(comboBox_referee3.getSelectedItem().toString());
+                System.out.println("Referee3 is " + refereeUser.get(2));
             }
         });
 
@@ -294,8 +297,8 @@ public class Controller extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // 获取裁判4
-                refereeUser[3] = comboBox_referee4.getSelectedItem().toString();
-                System.out.println("Referee4 is " + refereeUser[3]);
+                refereeUser.add(comboBox_referee4.getSelectedItem().toString());
+                System.out.println("Referee4 is " + refereeUser.get(3));
             }
         });
 
@@ -303,8 +306,8 @@ public class Controller extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // 获取裁判5
-                refereeUser[4] = comboBox_referee5.getSelectedItem().toString();
-                System.out.println("Referee5 is " + refereeUser[4]);
+                refereeUser.add(comboBox_referee5.getSelectedItem().toString());
+                System.out.println("Referee5 is " + refereeUser.get(4));
             }
         });
 
@@ -321,11 +324,14 @@ public class Controller extends JFrame {
         ensure.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                getOrder();
                 // 提交项目编号、年龄组到裁判处
                 try {
                     out.writeObject(TcpType.ScoreInstr.toString());
+                    out.writeObject("Luke,I am your father.");  // 控制人员账号
                     out.writeObject(mainRefereeUser);
                     out.writeObject(refereeUser);
+                    out.writeObject(sendOrderInfo);
                 } catch (IOException e1) {
                     // TODO Auto-generated catch block
                     e1.printStackTrace();
@@ -335,19 +341,17 @@ public class Controller extends JFrame {
         });
 
 
-
-
         splitePane.setLayout(null);
 
         splitePane.add(label_project);
         splitePane.add(label_age);
 
+        splitePane.add(label_chiefReferee);
         splitePane.add(label_referee1);
         splitePane.add(label_referee2);
         splitePane.add(label_referee3);
         splitePane.add(label_referee4);
         splitePane.add(label_referee5);
-        splitePane.add(label_chiefReferee);
         
         splitePane.add(comboBox_project);
         splitePane.add(comboBox_age);
